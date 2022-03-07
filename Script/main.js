93d90f1bfe2924425f54183420dc2ded3690035a
santiago.rosa <santi.rosa@hotmail.com>
2022-03-07 20:50:03 -0300

    Funciones Carrito JS

--- Script/main.js
+++ Script/main.js
@@ -1,56 +1,58 @@
-function carritouno(remeras, precioItem=0, disponibilidad, compra, envio) {
-const stockDisponible = validarQuiebre(disponibilidad, compra) ;
-if(stockDisponible) {
-    console.log(`añadido ${remeras} a ${precioItem} mas ${envio} de envío`);
+
+let stockactualRemeras = 20;
+let precioactualRemeras = 1500;
+let costoenvioRemeras = 500;
+
+function btnRemera_onclick() {
+    const stockDisponible = validarQuiebre(stockactualRemeras, 1); 
+    if(stockDisponible===true) {
+        stockactualRemeras = actualizarStock(stockactualRemeras, 1)
+        console.log(`añadido remeras a ${precioactualRemeras} mas ${costoenvioRemeras} de envío y queda stock ${stockactualRemeras}`);
     } else{
         console.log(`No disponible`);
-    }
+    }    
 }
 
-function validarQuiebre(disponibilidad, compra) {
-    if(disponibilidad > compra) {
+function validarQuiebre(stockactualRemeras, compra) {
+    if(stockactualRemeras > compra) {
         return true;
     } else{
         return false;
     }
 }
-carritouno(`remera`, 1500, 50, 10, 500);
 
-function carritodos(Buzos, precioItem=0, disponibilidad, compra, envio) {
-    const stockDisponible = validarQuiebre(disponibilidad, compra) ;
-    if(stockDisponible) {
-        console.log(`añadido ${Buzos} a ${precioItem} mas ${envio} de envío`);
-        } else{
-            console.log(`No disponible`);
-        }
-    }
-    
-    function validarQuiebre(disponibilidad, compra) {
-        if(disponibilidad > compra) {
-            return true;
-        } else{
-            return false;
-        }
-    }
+function actualizarStock(stockactualRemeras, compra) {
+    return (stockactualRemeras-compra);
+}
 
-    carritodos(`buzos`, 3000, 80, 20, 500);
+//-------------------------BUZOS---------------------------------------//
 
-    function carritotres(pantalones, precioItem=0, disponibilidad, compra, envio) {
-        const stockDisponible = validarQuiebre(disponibilidad, compra) ;
-        if(stockDisponible) {
-            console.log(`añadido ${pantalones} a ${precioItem} mas ${envio} de envío`);
-            } else{
-                console.log(`No disponible`);
-            }
-        }
-        
-        function validarQuiebre(disponibilidad, compra) {
-            if(disponibilidad > compra) {
-                return true;
-            } else{
-                return false;
-            }
-        }
+let stockactualBuzos = 11;
+let precioactualBuzos = 3000;
+let costoenvioBuzos = 500;
 
-carritotres(`pantalones`, 6000, 80, 20, 500)
+function btnBuzos_onclick() {
+    const stockDisponible = validarQuiebre(stockactualBuzos, 1); 
+    if(stockDisponible===true) {
+        stockactualBuzos = actualizarStock(stockactualBuzos, 1)
+        console.log(`añadido Buzos a ${precioactualBuzos} mas ${costoenvioBuzos} de envío y queda stock ${stockactualBuzos}`);
+    } else{
+        console.log(`No disponible`);
+    }    
+}
 
+//-------------------------PANTALONES---------------------------------------//
+
+let stockactualPantalones = 15;
+let precioactualPantalones = 5000;
+let costoenvioPantalones = 500;
+
+function btnPantalones_onclick() {
+    const stockDisponible = validarQuiebre(stockactualPantalones, 1); 
+    if(stockDisponible===true) {
+        stockactualPantalones = actualizarStock(stockactualPantalones, 1)
+        console.log(`añadido Pantalones a ${precioactualPantalones} mas ${costoenvioPantalones} de envío y queda stock ${stockactualPantalones}`);
+    } else{
+        console.log(`No disponible`);
+    }    
+}
