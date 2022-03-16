b18034acd0d272855ad839d14365a24d00d8399d
santiago.rosa <santi.rosa@hotmail.com>
2022-03-16 00:57:57 -0300

    solucion bug2

--- Script/main.js
+++ Script/main.js
@@ -1,102 +1,53 @@
+const STOCK_INICIAL_REMERAS = 5;
+const COSTO_ENVIO = 500;
+let stockRemeras = [];
+let carrito = [];
 
-let stockactualRemeras = 20;
-let precioactualRemeras = 1500;
-let costoenvioRemeras = 500;
+class Remera{
 
-function btnRemera_onclick() {
-    const stockDisponible = validarQuiebre(stockactualRemeras, 1); 
-    if(stockDisponible===true) {
-        stockactualRemeras = actualizarStock(stockactualRemeras, 1)
-        console.log(`añadido remeras a ${precioactualRemeras} mas ${costoenvioRemeras} de envío y queda stock ${stockactualRemeras}`);
-    } else{
-        console.log(`No disponible`);
-    }    
-}
-
-function validarQuiebre(stockactualRemeras, compra) {
-    if(stockactualRemeras > compra) {
-        return true;
-    } else{
-        return false;
+    constructor(color, precio){
+        this.color = color;
+        this.precio = precio;
     }
 }
 
-function actualizarStock(stockactualRemeras, compra) {
-    return (stockactualRemeras-compra);
+function inicializarStock(){
+    for(i=0;i<STOCK_INICIAL_REMERAS;i++){
+        stockRemeras.push(new Remera('blanca', 1000));
+        stockRemeras.push(new Remera('gris', 1500));
+        stockRemeras.push(new Remera('negra', 2000));
+        stockRemeras.push(new Remera('azul marino', 1000));
+    }
 }
 
-//-------------------------BUZOS---------------------------------------//
-
-let stockactualBuzos = 11;
-let stockactualBuzosAzulMarino = 15;
-let stockactualBuzosGris = 25;
-let precioactualBuzos = 3000;
-let precioactualBuzosAzulMarino = 3000;
-let precioactualBuzosGris = 3000;
-let costoenvioBuzos = 500;
-
-function btnBuzos_onclick_blanco() {
-    const stockDisponible = validarQuiebre(stockactualBuzos, 1); 
-    if(stockDisponible===true) {
-        stockactualBuzos = actualizarStock(stockactualBuzos, 1)
-        console.log(`añadido Buzos a ${precioactualBuzos} mas ${costoenvioBuzos} de envío y queda stock ${stockactualBuzos}`);
-    } else{
-        console.log(`No disponible`);
-    }    
+function validarStock(color){
+    return stockRemeras.find(element => element.color === color);
 }
 
-function btnBuzos_onclick_azul_marino() {
-    const stockDisponible = validarQuiebre(stockactualBuzosAzulMarino, 1); 
-    if(stockDisponible===true) {
-        stockactualBuzosAzulMarino = actualizarStock(stockactualBuzosAzulMarino, 1)
-        console.log(`añadido Buzos a ${precioactualBuzosAzulMarino} mas ${costoenvioBuzos} de envío y queda stock ${stockactualBuzosAzulMarino}`);
-    } else{
-        console.log(`No disponible`);
-    }    
+function agregarCarrito(producto){
+    carrito.push(producto);
 }
 
-function btnBuzos_onclick_gris() {
-    const stockDisponible = validarQuiebre(stockactualBuzosGris, 1); 
-    if(stockDisponible===true) {
-        stockactualBuzosGris = actualizarStock(stockactualBuzosGris, 1)
-        console.log(`añadido Buzos a ${precioactualBuzosGris} mas ${costoenvioBuzos} de envío y queda stock ${stockactualBuzosGris}`);
-    } else{
-        console.log(`No disponible`);
-    }    
+function reducirStock(producto){
+    index = stockRemeras.indexOf(producto);
+    if (index != -1){
+        stockRemeras.splice(index, 1);
+    }
 }
 
-//-------------------------PANTALONES---------------------------------------/
-// const producto = {
-//     titulo: `jean`,  
-//     precio: 6000,
-//     categoria: `pantalones`,
-// };
-
-const PRODUCTO = new Producto(`jean`, 6000, `pantalones`);
-const PRODUCTO2 = new Producto2(`gabardina`, 6000, `pantalones`);
-const PRODUCTO3 = new Producto3(`jogger`, 6000, `pantalones`);
-
-agregarAlCarrito(PRODUCTO);
-agregarAlCarrito(PRODUCTO2);
-agregarAlCarrito(PRODUCTO3);
-//-------------------------declaracion fx constructora---------------------------------------/
-function Producto(titulo, precio, categoria){
-    this.titulo = titulo;
-    this.precio = precio;
-    this.categoria = categoria;
-}
-function Producto2(titulo, precio, categoria){
-    this.titulo = titulo;
-    this.precio = precio;
-    this.categoria = categoria;
-}
-function Producto3(titulo, precio, categoria){
-    this.titulo = titulo;
-    this.precio = precio;
-    this.categoria = categoria;
+function btnRemera_onClick(color) {
+    //validar que existe remera de color en stock
+    const remera = validarStock(color);
+    if (remera != undefined){
+        reducirStock(remera);
+        agregarCarrito(remera);
+    }else{
+        alert('No hay mas remeras de color ' + color);
+    }
 }
 
-function agregarAlCarrito(producto) {
-    const leyenda = `Añadiste ${producto.titulo} - $ ${producto.precio}`;
-    console.log(leyenda);
-};
+function totalCarrito(){
+    let total = 0;
+    carrito.forEach(element => total+=element.precio);
+    return total + COSTO_ENVIO;
+}
