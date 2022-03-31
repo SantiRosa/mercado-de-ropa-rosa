757b7a24b88ef24bd80bab99c9706452ada086d8
Santiago Rosa <santi.rosa@hotmail.com>
2022-03-30 21:54:54 -0300

    modifico formulas. Agrego cantidades compradas y subtotales

--- Script/main.js
+++ Script/main.js
@@ -1,18 +1,20 @@
 const agregarAlCarrito = (producto) => {
-    carrito.push(producto);
+    let productocarrito;
+    productocarrito = producto;
+    carrito.push(productocarrito);
 };
 
 const carrito = [];
 
 const productos = [
-    { id: 1, titulo: "Remera Blanca", precio: 1000, stock: 0, imagen: "../imagenes/remerablanca.jpg"},
-    { id: 2, titulo: "Remera Negra", precio: 1000, stock: 15, imagen: "../imagenes/remeranegra.jpg" },     
-    { id: 3, titulo: "Remera Gris", precio: 1000, stock: 5, imagen: "../imagenes/remeragris.jpg" },
-    { id: 4, titulo: "Remera Azul", precio: 1000, stock: 5, imagen: "../imagenes/remeraazulmarino.jpg" },
-    { id: 5, titulo: "Buzo Blanco", precio: 3000, stock: 8, imagen: "../imagenes/buzoblanco.jpg" },
-    { id: 6, titulo: "Buzo Gris", precio: 3000, stock: 9, imagen: "../imagenes/buzogris.jpg" },
-    { id: 7, titulo: "Buzo Negro", precio: 3000, stok: 4, imagen: "../imagenes/buzonegro.jpg" },
-    { id: 8, titulo: "Buzo Azul", precio: 3000, stok: 0, imagen: "../imagenes/buzoazulmarino.jpg" },
+    { id: 1, titulo: "Remera Blanca", precio: 1000, stock: 0, cantidadComprada: 0, subtotalCompra: 0, imagen: "../imagenes/remerablanca.jpg"},
+    { id: 2, titulo: "Remera Negra", precio: 1000, stock: 15, cantidadComprada: 0, subtotalCompra: 0, imagen: "../imagenes/remeranegra.jpg" },     
+    { id: 3, titulo: "Remera Gris", precio: 1000, stock: 5, cantidadComprada: 0, subtotalCompra: 0, imagen: "../imagenes/remeragris.jpg" },
+    { id: 4, titulo: "Remera Azul", precio: 1000, stock: 5, cantidadComprada: 0, subtotalCompra: 0, imagen: "../imagenes/remeraazulmarino.jpg" },
+    { id: 5, titulo: "Buzo Blanco", precio: 3000, stock: 8, cantidadComprada: 0, subtotalCompra: 0, imagen: "../imagenes/buzoblanco.jpg" },
+    { id: 6, titulo: "Buzo Gris", precio: 3000, stock: 9, cantidadComprada: 0, subtotalCompra: 0, imagen: "../imagenes/buzogris.jpg" },
+    { id: 7, titulo: "Buzo Negro", precio: 3000, stok: 4, cantidadComprada: 0, subtotalCompra: 0, imagen: "../imagenes/buzonegro.jpg" },
+    { id: 8, titulo: "Buzo Azul", precio: 3000, stok: 0, cantidadComprada: 0, subtotalCompra: 0, imagen: "../imagenes/buzoazulmarino.jpg" },
 ]
 
 generarCards(productos);
@@ -28,7 +30,7 @@ function generarCards(productosAmostrar){
           <div class="card-body">
             <h5 class="card-title">${productos.titulo}</h5>
             <p class="card-text">${productos.precio}</p>
-            <button class="btn btn-outline-success carritouno" onclick="btnRemera_onClick('blanca')">Añadir</button>
+            <button class="btn btn-outline-success carritouno" onclick="btnCompra_onClick('${productos.titulo}')">Añadir</button>
           </div>
         </div>
         </div>  
@@ -49,4 +51,67 @@ function filtrarProducto() {
     });
 
     generarCards(remerasFiltradas);
+}
+
+function btnCompra_onClick(producto) {
+    let elemento = encontrarProducto(producto);
+    let existencias = validarStock(elemento);
+    if(existencias == true){
+        let hayEnCarrito = existeEnCarrito(elemento);
+        if(hayEnCarrito == true){
+            let elementoEnCarrito = encontrarProductoEnCarrito(producto);
+            elementoEnCarrito.cantidadComprada = elementoEnCarrito.cantidadComprada + 1;
+            elemento.stock = elemento.stock - 1;
+            elementoEnCarrito.subtotalCompra = elementoEnCarrito.cantidadComprada * elementoEnCarrito.precio;
+        }
+        else{
+            agregarAlCarrito(elemento);
+            let elementoEnCarrito = encontrarProductoEnCarrito(producto);
+            elementoEnCarrito.cantidadComprada = elementoEnCarrito.cantidadComprada + 1;
+            elemento.stock = elemento.stock - 1;
+            elementoEnCarrito.subtotalCompra = elementoEnCarrito.cantidadComprada * elementoEnCarrito.precio;
+        }
+    }
+    else {
+    }
+}
+
+function encontrarProducto(productoTitulo) {
+    let productoEncontrado;
+    productos.forEach(productoSeleccionado => {
+        if(productoTitulo == productoSeleccionado.titulo){
+            productoEncontrado = productoSeleccionado;
+        }
+    });
+    return productoEncontrado;
+}
+
+function encontrarProductoEnCarrito(productoTitulo) {
+    let productoEncontrado;
+    carrito.forEach(productoSeleccionado => {
+        if(productoTitulo == productoSeleccionado.titulo){
+            productoEncontrado = productoSeleccionado;
+        }
+    });
+    return productoEncontrado;
+}
+
+
+function validarStock(productoEncontrado) {
+    if (productoEncontrado.stock > 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+function existeEnCarrito(productoClick) {
+    let existeEnCarro = false;
+    carrito.forEach(productoEnCarro => {
+        if(productoClick.titulo == productoEnCarro.titulo){
+            existeEnCarro = true;
+        }
+    });
+    return existeEnCarro;
 }
